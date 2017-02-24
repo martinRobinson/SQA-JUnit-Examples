baecd67f4fc99d2618bbd086eace49a0e531eb14
Martin Robinson <martin.robinson@lyit.ie>
2017-02-24 16:54:07 +0000

    Another thrown exception Junit test for the setAge method.

--- src/test/java/personTester.java
+++ src/test/java/personTester.java
@@ -1,17 +1,22 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import java.util.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class personTester {
-    private person testDummy = new person("Joe", "Bloggs", 21);
-    
-    @Test
-    public void testGetAge(){
-        assertEquals(21, testDummy.getAgeInYears());
-    }
+	private person testDummy = new person("Joe", "Bloggs", 21);
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetAgeInYearsTooSmallByThrownException() {
-        testDummy.setAgeInYears(-1);
-    }
+	@Test
+	public void testGetAge() {
+		assertEquals(21, testDummy.getAgeInYears());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetAgeInYearsTooSmallByThrownException() {
+		testDummy.setAgeInYears(-1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetAgeInYearsTooBigByThrownException() {
+		testDummy.setAgeInYears(131);
+	}
 }
