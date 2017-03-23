cdc803ab630a60b8412995e5d4e9f25e9aae54b1
Martin Robinson <martin.robinson@lyit.ie>
2017-03-23 10:28:23 +0000

    Added the AssertJ testing framework to the project to experiment with more fluent testing assertions. Added a few tests for the person class.

---
+++ src/test/java/personTesterAssertJ.java
@@ -0,0 +1,22 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class personTesterAssertJ {
+	private person testDummy = new person("Joe", "Bloggs", 21);
+
+	@Test
+	public void testGetAgeTypical() {
+		assertThat(testDummy.getAgeInYears()).isEqualTo(21);
+	}
+
+	@Test
+	public void testCorrectFirstName() {
+		assertThat(testDummy.getFirstName()).isEqualTo("Joe");
+	}
+
+	@Test
+	public void testCorrectLastName() {
+		assertThat(testDummy.getLastName()).isEqualTo("Bloggs");
+	}
+}
